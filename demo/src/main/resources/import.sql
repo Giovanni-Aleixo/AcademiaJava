98071ae7227290df906bc0330b309696043ad043
Giovanni Aleixo <147216732+Giovanni-Aleixo@users.noreply.github.com>
2023-11-26 21:11:55 -0300

    Add files via upload

---
+++ demo/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO tb_department(name) VALUES ('Gestão');
+INSERT INTO tb_department(name) VALUES ('Informática');
+
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Giovanni', 'giovanni@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (1, 'Joao', 'joao@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Nicole', 'nicole@gmail.com');
+INSERT INTO tb_user(department_id, name, email) VALUES (2, 'Ana', 'ana@gmail.com');
